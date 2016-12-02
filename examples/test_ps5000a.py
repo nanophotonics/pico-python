89427aa45145bcbb9ca1863aeaad37abfe44a7eb
anaandresarroyo <aa938@cam.ac.uk>
2016-12-02 12:39:11 +0000

    Implement autoscale of each channel range

--- examples/test_ps5000a.py
+++ examples/test_ps5000a.py
@@ -3,189 +3,103 @@ PS5444B Test.
 By: Ana Andres-Arroyo
 """
 
-import picoscope
+#import picoscope
 import matplotlib.pyplot as plt
 import numpy as np
 import time
 from ctypes import *
 
-picoscope = reload(picoscope)
-from picoscope import ps5000a
-ps5000a = reload(ps5000a)
+#picoscope = reload(picoscope)
+#from picoscope import ps5000a
+#ps5000a = reload(ps5000a)
+from picoscope import PicoScope5000a
 
 if __name__ == "__main__":
     print(__doc__)
-
-    print("Attempting to open Picoscope 5000a...")
-
-#    SERIAL_NUM = 'CS960/049'
-#    ps = ps5000a.PS5000a(SERIAL_NUM) # specity the serial number
-    ps = ps5000a.PS5000a() # automatically detect the serial number 
     
-    print("\nFound the following picoscope:")
-    print(ps.getAllUnitInfo() + "\n")
+    """Instantiate the picoscope object"""
+    ps = PicoScope5000a.PicoScope5000a()
     
-
+    """Specify which channels to use."""
+    ps_channels = ['A', 'B']
+#    ps_channels = ['B']    
     
-    """
-    Set up a specific channel.
-
-    It finds the smallest range that is capable of accepting your signal.
-    Return actual range of the scope as double.
-
-    The VOffset, is an offset that the scope will ADD to your signal.
-
-    If using a probe (or a sense resitor), the probeAttenuation value is used 
-    to find the approriate channel range on the scope to use.
-
-    e.g. to use a 10x attenuation probe, you must supply the following 
-    parameters ps.setChannel('A', "DC", 20.0, 5.0, True, False, 10.0)
-
-    The scope will then be set to use the +- 2V mode at the scope allowing you 
-    to measure your signal from -25V to +15V.
-    After this point, you can set everything in terms of units as seen at the 
-    tip of the probe. For example, you can set a trigger of 15V and it will 
-    trigger at the correct value.
-
-    When using a sense resistor, lets say R = 1.3 ohm, you obtain the relation:
-    V = IR, meaning that your probe as an attenuation of R compared to the 
-    current you are trying to measure.
-
-    You should supply probeAttenuation = 1.3
-    The rest of your units should be specified in amps.
-
-    Unfortunately, you still have to supply a VRange that is very close to the 
-    allowed values. This will change in furture version where we will find the 
-    next largest range to accomodate the desired range.
-
-    If you want to use units of mA, supply a probe attenuation of 1.3E3.
-    Note, the authors recommend sticking to SI units because it makes it easier 
-    to guess what units each parameter is in.
-
-    """
-    channelRangeA = ps.setChannel(channel="A",                                  
-                                 coupling="DC", 
-                                 VRange=10.0, 
-                                 VOffset=0.0,
-                                 enabled=True,                                  
-                                 BWLimited=False,
-                                 probeAttenuation=1.0,
-                                 )
-    print("A channel range = %d V" % channelRangeA)
-    
-    channelRangeB = ps.setChannel(channel="B",                                  
-                                 coupling="DC", 
-                                 VRange=10.0, 
-                                 VOffset=0.0,
-                                 enabled=True,                                  
-                                 BWLimited=False,
-                                 probeAttenuation=1.0,
-                                 )
-    print("B channel range = %d V" % channelRangeA)
-    
-    """
-    Sampling interval and timebase setup.
-    """
-    waveform_duration = 600E-3 # seconds
-    sampling_interval = waveform_duration / 4096 # where does 4096 number come from?
-
+    """Sampling interval and timebase setup."""    
+    waveform_duration = 1.2 # seconds
+    number_of_samples = 2**11
+    sampling_interval = waveform_duration / number_of_samples
     (actualSamplingInterval, nSamples, maxSamples) = \
         ps.setSamplingInterval(sampling_interval, waveform_duration)
+        
+    print("Waveform duration = %f s" % (nSamples * actualSamplingInterval))
     print("Sampling interval = %f ns" % (actualSamplingInterval * 1E9))
     print("Taking  samples = %d" % nSamples)
     print("Maximum samples = %d" % maxSamples)
+    print('\n')
 
-    """
-    Simple Trigger setup.
-
-    trigSrc can be either a number corresponding to the low level
-    specifications of the scope or a string such as 'A' or 'AUX'
-
-    direction can be a text string such as "Rising" or "Falling",
-    or the value of the dict from self.THRESHOLD_TYPE[] corresponding
-    to your trigger type.
-
-    delay is number of clock cycles to wait from trigger conditions met
-    until we actually trigger capture.
-
-    timeout_ms is time to wait in mS from calling runBlock() or similar
-    (e.g. when trigger arms) for the trigger to occur. If no trigger
-    occurs it gives up & auto-triggers.
-
-    Support for offset is currently untested
-
-    Note, the AUX port (or EXT) only has a range of +- 1V (at least in PS6000)
-    """
-    ps.setSimpleTrigger(trigSrc='A', 
-                        threshold_V=0.5, 
+    """Simple Trigger setup."""
+    ps.setSimpleTrigger(trigSrc=ps_channels[0], 
+                        threshold_V=0.002, 
                         direction='Rising', 
                         delay=0,
                         timeout_ms=100, 
                         enabled=True
                         )
 
-
-    """
-    Block mode setup. 
-    
-    In block mode, the computer prompts a PicoScope 5000 Series oscilloscope to 
-    collect a block of data into its internal memory. When the oscilloscope has 
-    collected the whole block, it signals that it is ready and then transfers 
-    the whole block to the computer's memory through the USB port.
-    """    
-    
-    """
-    Rapid block mode setup. 
-    
-    Rapid block mode allows you to sample several waveforms at a time with the
-    minimum time between waveforms. It reduces the gap from milliseconds to 
-    less than 2 microseconds (on fastest timebase).
-    """ 
-
-    """
-    This function sets the number of captures to be collected in one run of 
-    rapid block mode. If you do not call this function before a run, the driver 
-    will capture only one waveform. Once a value has been set, the value 
-    remains constant unless changed.
-    """    
+  
+    """Rapid block mode setup."""   
     number_of_captures = 1
-    ps.setNoOfCaptures(number_of_captures)
+    ps.setNoOfCaptures(number_of_captures)    
     
-    """
-    Streaming mode setup.
     
-    Streaming mode can capture data without the gaps that occur between blocks 
-    when using block mode. Streaming mode supports downsampling and triggering, 
-    while providing fast streaming at up to 31.25 MS/s (32 ns per sample) when 
-    one channel is active, depending on the computer's performance. This makes 
-    it suitable for high-speed data acquisition, allowing you to capture long 
-    data sets limited only by the computer's memory.
+    ps_channels_range = []
+    ps_data = []                                                         
+    for i in range(len(ps_channels)):
+        ps_channels_range.append(0.02)
+        ps_data.append([])
+        
+        data = [0.0, 0.0] 
+        channelRange = ps_channels_range[i]            
+        while max(data) < 32512*0.3 or \
+              max(data) > 32512*0.95:
+                        
+            """Set up channel range."""
+            channelRange = ps.setChannel(channel=ps_channels[i],
+                                 coupling="DC", 
+                                 VRange=channelRange, 
+                                 VOffset=0.0,
+                                 enabled=True,                                  
+                                 BWLimited=False,
+                                 probeAttenuation=1.0,
+                                 ) 
+            print ps_channels[i] + ' channel range = ' + str(channelRange) + ' V'
+            ps_channels_range[i] = channelRange
     
-    TO DO! The low level functions in ps5000a.py are mostly empty
-    """
-    
-    ps.runBlock()
-    ps.waitReady()
-    (dataA, numSamplesReturnedA, overflowA) = ps.getDataRaw(channel='A', 
-                                                         numSamples=0, 
-                                                         startIndex=0, 
-                                                         downSampleRatio=1,
-                                                         downSampleMode=0, 
-                                                         segmentIndex=0, 
-                                                         data=None,
-                                                         )
-                                                         
-    (dataB, numSamplesReturnedB, overflowB) = ps.getDataRaw(channel='B', 
-                                                         numSamples=0, 
-                                                         startIndex=0, 
-                                                         downSampleRatio=1,
-                                                         downSampleMode=0, 
-                                                         segmentIndex=0, 
-                                                         data=None,
-                                                         )
-    
-    plt.plot(dataA)
-    plt.plot(dataB)
+            """Set up data collection."""
+            ps.runBlock()
+            ps.waitReady()
+                  
+            """Collect data."""
+            (data, numSamplesReturnedA, overflow) = \
+                ps.getDataRaw(channel=ps_channels[i])
+            print 'Maximum value = ' + str(max(data)) + '\n'
+            
+            """Modify chanel range if data is too small/big."""
+            if max(data) < 32512*0.3:
+                channelRange = channelRange / 2
+            elif max(data) > 32512*0.95:
+                channelRange = channelRange * 2  
+            
+        """Convert data to volts."""
+        data = data/32512.0*ps_channels_range[i]
+        
+        """Append data to list."""
+        ps_data[i].append(data)
+        
+        """Plot data."""
+        plt.plot(data)
+        
+    ps.close()
 
 #    
 #    c = 3e8
@@ -261,4 +175,4 @@ if __name__ == "__main__":
 #    plt.colorbar()
 #    plt.show()
     
-    ps.close()
+    
